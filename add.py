f523584756e5e4f797fdeab8628d6ded2be3f20a
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2021-08-28 10:58:13 +0530

    Revert "Add new functions to find sum and product of 3 numbers"
    This reverts commit c746ab57ced6b1345a60118498a384eb2ffa4cca.

--- add.py
+++ add.py
@@ -2,8 +2,4 @@ def add2(num1, num2):
     sum = num1 + num2
     return sum
 
-def add3(num1, num2, num3):
-    sum = num1 + num2 + num3
-    return sum
-
-print(add3(3, 4, 8))
+print(add2(3, 4))
--- multiply.py
+++ multiply.py
@@ -1,9 +1,5 @@
-def multiply2(num1, num2):
+def multipy2(num1, num2):
     product = num1 * num2
-    return product
-
-def multiply3(num1, num2, num3):
-    product = num1 * num2 * num3
     return product
 
 print(multiply2(4, 5))
